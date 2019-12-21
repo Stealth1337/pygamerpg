23b060e53de172238edfcc3a353e4cf94783c9f4
Stealth1337 <noobdota2281@gmail.com>
2019-12-21 12:32:26 +0300

    - Полный реворк системы передвижения (пока что без зажатия клавиши) - Новые классы игрока/локации.

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
 import pygame
-from room import Room
+from location import Location
+from player import Player
+
 pygame.init()
 
-width, height = 1280, 720
+size_screen = width, height = 800, 600
+main_screen = pygame.display.set_mode(size_screen)
+running_game = True
 
-screen = pygame.display.set_mode((width, height))
-fps = 20
-clock = pygame.time.Clock()
-running = True
-c = Room(10, 10, 10, 10)
-c.board[0][0] = 1
-current_x = 0
-current_y = 0
-while running:
-    screen.fill((0, 0, 0))
-    c.render(screen)
-    pygame.display.flip()
+location_1 = True
+loc_1 = Location(0, 0, 5, 5)
+location_screen = pygame.Surface((loc_1.width * 50, loc_1.height * 50))
+loc_1.create(location_screen)
+player = Player(20, 20, 'red')
+
+while running_game:
+    main_screen.fill((0, 0, 0))
+    player.renderPlayer(location_screen)
+    main_screen.blit(location_screen, (10, 10))
+    location_screen.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running_game = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
-                c.board[current_x][current_y] = 0
-                c.board[current_x][current_y + 1] = 1
-                current_y += 1
-            elif event.key == pygame.K_s:
-                c.board[current_x][current_y] = 0
-                c.board[current_x + 1][current_y] = 1
-                current_x += 1
-            elif event.key == pygame.K_a:
-                c.board[current_x][current_y] = 0
-                c.board[current_x][current_y - 1] = 1
-                current_y -= 1
-            elif event.key == pygame.K_w:
-                c.board[current_x][current_y] = 0
-                c.board[current_x - 1][current_y] = 1
-                current_x -= 1
-
-
-    clock.tick(fps)
+                player.move(event.key, [loc_1.start_x, loc_1.start_y, loc_1.end_x, loc_1.end_y])
+                print(player.current_x)
+                print(loc_1.end_x)
+    pygame.display.flip()
+pygame.quit()
