48e55b842cecbb6a11a32354428398c6988701a1
Stealth1337 <noobdota2281@gmail.com>
2019-12-23 18:56:55 +0300

    - Немного изменена система координат для спрайта. (то же самое, что) и для Player()

--- barriers.py
+++ barriers.py
@@ -2,12 +2,15 @@ import pygame
 
 
 class Barrier(pygame.sprite.Sprite):
+
     def __init__(self, x, y, width, height):
         pygame.sprite.Sprite.__init__(self)
-        self.x = x
-        self.y = y
-        self.width = width * 50
         self.height = height * 50
+        self.width = width * 50
+        self.image = pygame.Surface((self.width, self.height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
     def render(self, surface, color):
-        pygame.draw.rect(surface, color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(surface, color, (self.rect.x, self.rect.y, self.width, self.height))
