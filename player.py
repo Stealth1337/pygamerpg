98e59ac6d4be26cf856a54a5332e36eeb1680aef
Stealth1337 <noobdota2281@gmail.com>
2019-12-23 18:56:29 +0300

    - Немного изменена система координат для спрайта.

--- player.py
+++ player.py
@@ -5,25 +5,27 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, start_x, start_y, color):
         pygame.sprite.Sprite.__init__(self)
-        self.current_x = start_x
-        self.current_y = start_y
         self.player_color = pygame.Color(color)
+        self.image = pygame.Surface((30, 30))
+        self.rect = self.image.get_rect()
+        self.rect.x = start_x
+        self.rect.y = start_y
 
     def renderPlayer(self, surface):
-        pygame.draw.rect(surface, self.player_color, (self.current_x, self.current_y, 30, 30))
+        pygame.draw.rect(surface, self.player_color, (self.rect.x, self.rect.y, 30, 30))
 
     def move(self, event, restriction_list):
-        print(restriction_list[3])
-        if event == 273:
-            if restriction_list[1] < self.current_y:
-                self.current_y -= 5
-        elif event == 274:
-            if self.current_y < restriction_list[3] - 30:
-                self.current_y += 5
-                print(self.current_y)
-        elif event == 276:
-            if restriction_list[0] < self.current_x:
-                self.current_x -= 5
-        elif event == 275:
-            if self.current_x < restriction_list[2] - 30:
-                self.current_x += 5
+        if event == pygame.K_UP:
+            if restriction_list[1] < self.rect.y:
+                self.rect.y -= 5
+
+                print(self.rect.x, self.rect.y)
+        elif event == pygame.K_DOWN:
+            if self.rect.y < restriction_list[3] - 30:
+                self.rect.y += 5
+        elif event == pygame.K_LEFT:
+            if restriction_list[0] < self.rect.x:
+                self.rect.x -= 5
+        elif event == pygame.K_RIGHT:
+            if self.rect.x < restriction_list[2] - 30:
+                self.rect.x += 5
